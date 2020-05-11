c1c35349d60799ea991ea075a3d6d8eec3491eaf
Seith Miller <seithmiller@gmail.com>
2020-05-11 18:46:45 -0400

    1

---
+++ gopher.go
@@ -0,0 +1,24 @@
+package main
+
+import(
+  "fmt"
+  "time"
+)
+
+func main(){
+  fmt.Println("    `.-::::::-.`    ")
+  fmt.Println(".:-::::::::::::::-:.")
+  fmt.Println("`_:::    ::    :::_`")
+  fmt.Println(" .:( ^   :: ^   ):. ")
+  fmt.Println(" `:::   (..)   :::. ")
+  fmt.Println(" `:::::::UU:::::::` ")
+  fmt.Println(" .::::::::::::::::. ")
+  fmt.Println(" O::::::::::::::::O ")
+  fmt.Println(" -::::::::::::::::- ")
+  fmt.Println(" `::::::::::::::::` ")
+  fmt.Println("  .::::::::::::::.  ")
+  fmt.Println("    oO:::::::Oo     ")
+  fmt.Println(time.Now())
+}
+
+
