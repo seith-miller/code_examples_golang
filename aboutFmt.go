de29e5ba2876c62f9aa71d22cf92005210d48c0e
Seith Miller <seithmiller@gmail.com>
2020-05-12 15:21:29 -0400

    add about fmt

---
+++ aboutFmt.go
@@ -0,0 +1,30 @@
+/*
+Starting Point:
+
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("Let's first see how", "the Println() method works.")
+  fmt.Println("Notice that each statement adds a newline for us.")
+  fmt.Println("There's also a default space", "between the string arguments.")
+  
+  // Add your code below:
+  
+}
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("Let's first see how", "the Println() method works.")
+  fmt.Println("Notice that each statement adds a newline for us.")
+  fmt.Println("There's also a default space", "between the string arguments.")
+  
+  // Add your code below:
+  fmt.Print("Print", "is", "different")
+  fmt.Print("See?")
+}
