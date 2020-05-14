c473b2416d47e02166954ce9e1609a2606082145
Seith Miller <seithmiller@gmail.com>
2020-05-14 12:00:25 -0400

    add interstellerTravel

---
+++ interstellarTravel.go
@@ -0,0 +1,95 @@
+/*
+Starting Point:
+package main
+
+import "fmt"
+
+// Create the function fuelGauge() here
+
+
+// Create the function calculateFuel() here
+
+
+// Create the function greetPlanet() here
+
+
+// Create the function cantFly() here
+
+
+// Create the function flyToPlanet() here
+
+
+func main() {
+  // Test your functions!
+  
+  // Create `planetChoice` and `fuel`
+  
+  // And then liftoff!
+  
+}
+*/
+
+package main
+
+import "fmt"
+
+// Create the function fuelGauge() here
+func fuelGauge(fuel int){
+  fmt.Printf("You have %d fuel left!\n", fuel)
+}
+
+// Create the function calculateFuel() here
+func calculateFuel(planet string) int{
+  var fuel int
+  switch planet{
+    case "Venus": 
+      fuel = 300000
+    case "Mercury":
+      fuel = 500000
+    case "Mars":
+      fuel = 700000
+    default:
+      fuel = 0 
+  }
+  return fuel
+}
+
+// Create the function greetPlanet() here
+func greetPlanet(planet string) {
+  fmt.Println("Welcome to", planet, "!")
+}
+
+// Create the function cantFly() here
+func cantFly(){
+  fmt.Println("We do not have the available fuel to fly there.")
+}
+
+// Create the function flyToPlanet() here
+func flyToPlanet(planet string, fuel int) int{
+  var fuelRemaining int
+  var fuelCost int
+
+  fuelRemaining = fuel
+  fuelCost = calculateFuel(planet)
+
+  if fuelRemaining > fuelCost {
+    greetPlanet(planet)
+    fuelRemaining -= fuelCost
+  }
+
+  if fuelCost > fuelRemaining {
+    cantFly()
+  }
+
+  return fuelRemaining
+}
+
+func main() {
+  // Create `planetChoice` and `fuel`
+  fuel := 1000000
+  planetChoice := "Venus"
+
+  // And then liftoff!
+  fuel = flyToPlanet(planetChoice, fuel)
+  fuelGauge(fuel)
+}
