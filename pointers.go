9759d70922673d61466f034dd5e9d2d2f458d692
Seith Miller <seithmiller@gmail.com>
2020-05-14 12:30:01 -0400

    add pointers

---
+++ pointers.go
@@ -0,0 +1,41 @@
+/*
+starting point:
+
+package main
+
+import "fmt"
+
+// Change brainwash to have a pointer parameter
+func brainwash(saying string) {
+	// Dereference saying below: 
+	saying = "Beep Boop."
+}
+
+func main() {
+	greeting := "Hello there!"
+	
+	// Call your brainwash() below:
+	
+	
+	fmt.Println("greeting is now:", greeting)
+}
+*/
+
+package main
+
+import "fmt"
+
+// Change brainwash to have a pointer parameter
+func brainwash(saying *string) {
+	// Dereference saying below: 
+	*saying = "Beep Boop."
+}
+
+func main() {
+	greeting := "Hello there!"
+	
+	// Call your brainwash() below:
+  	brainwash(&greeting)
+
+	fmt.Println("greeting is now:", greeting)
+}
